75669fa211340a0fcd5c4ad023bfb32abcdc471d
Nikita Alutis <nikalutis@yandex.ru>
2021-04-15 23:05:01 +0300

    clearing

--- model_sql_interpreter_2021/Table.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a73439e580a1069ed29847de5966ab9cac727fb959a50b07e67ce3d5cc4bc4cd
-size 7491
--- model_sql_interpreter_2021/socket.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a261f11580a8a743401c5b47b1ff8b4a9549935a3d4cb580a1c9da2542196b50
-size 3323
