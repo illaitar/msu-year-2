e4660ff638c93995a068f53fdf7423031ce7641d
Nikita Alutis <nikalutis@yandex.ru>
2021-04-15 23:03:31 +0300

    added sql interpreter

---
+++ model_sql_interpreter_2021/client.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f6cd82c8d8ed4ce98d3fd4a74661466d230d000bf2b4c960abdfc4d2c9143706
+size 14893
---
+++ model_sql_interpreter_2021/server.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:913cfc8bb62bc8c7d340156b6163153fd71bab4ee897fbf795f4e4fbb2359b78
+size 1786
